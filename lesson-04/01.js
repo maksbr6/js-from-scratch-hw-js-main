14f88b46f5ec3a230e41a2619e2577969a2afa50
maksbr6 <maks_2br@mail.ru>
2025-08-26 16:51:16 +0300

    lesson 04-01

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -12,5 +12,12 @@
 Пример использования:
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
-
-function includesElement() {}
+function includesElement(array, element) {
+    let isElementInArray = false
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            isElementInArray = true
+        }
+    }
+    return isElementInArray
+}
