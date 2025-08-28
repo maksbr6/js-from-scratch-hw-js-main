c49687095fb0d9a6cbf1ce884bdcca875ab4b083
maksbr6 <maks_2br@mail.ru>
2025-08-28 14:47:54 +0300

    lesson 05-01

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,11 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resource, amount) {
+    if (resource in game.resources === false) {
+      console.log("Invalid resource");
+    } else {
+      game.resources[resource] = game.resources[resource] + amount
+    }
+  }
 }
